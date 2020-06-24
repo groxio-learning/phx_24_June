6d6aadb7218d9ca6acdbcdafdd31b9e9d9324b97
Duncan Sparrell <duncan@sfractal.com>
2020-06-24 15:40:12 -0400

    Gettysburg Address

--- recuerdo/lib/recuerdo/speech.ex
+++ recuerdo/lib/recuerdo/speech.ex
@@ -1,8 +1,18 @@
 defmodule Speech do
-  
+
   def passage("macbeth") do
     """
     tomorrow, tomorrow, and tomorrow...
+    """
+  end
+  def passage("gettysburg") do
+    """
+    Four score and seven years ago
+    our fathers brought forth on this continent,
+    a new nation,
+    conceived in Liberty,
+    and dedicated to the proposition
+    that all men are created equal.
     """
   end
 end
