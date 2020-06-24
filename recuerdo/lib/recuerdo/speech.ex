0a6604f5617bbb14990b4310f150d4cdc8d5b73d
J. Daniel Ashton <jdashton@ashtonfam.org>
2020-06-24 15:37:14 -0400

    Add preamble speech

--- recuerdo/lib/recuerdo/speech.ex
+++ recuerdo/lib/recuerdo/speech.ex
@@ -1,5 +1,15 @@
 defmodule Speech do
-  
+  def passage("preamble") do
+    """
+    When in the Course of human events, it becomes necessary for one people to
+    dissolve the political bands which have connected them with another, and to
+    assume among the powers of the earth, the separate and equal station to
+    which the Laws of Nature and of Nature's God entitle them, a decent respect
+    to the opinions of mankind requires that they should declare the causes
+    which impel them to the separation.
+    """
+  end
+
   def passage("macbeth") do
     """
     Tomorrow, and tomorrow, and tomorrow,
