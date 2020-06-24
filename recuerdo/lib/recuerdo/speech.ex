e5471ddf5314ab2f205bd6d8531d2f11b7c4ee34
James MacAulay <jamesmacaulay@users.noreply.github.com>
2020-06-24 15:45:49 -0400

    add Who goes with Fergus poem

--- recuerdo/lib/recuerdo/speech.ex
+++ recuerdo/lib/recuerdo/speech.ex
@@ -1,8 +1,26 @@
 defmodule Speech do
-  
   def passage("macbeth") do
     """
     tomorrow, tomorrow, and tomorrow...
+    """
+  end
+
+  # Who goes with Fergus? — W.B. Yeats
+  def passage("fergus") do
+    """
+    Who will go drive with Fergus now,
+    And pierce the deep wood's woven shade,
+    And dance upon the level shore?
+    Young man, lift up your russet brow,
+    And lift your tender eyelids, maid
+    And brood on hopes and fear no more.
+
+    And no more turn aside and brood
+    Upon love's bitter mystery;
+    For Fergus rules the brazen cars,
+    And rules the shadows of the wood,
+    And the white breast of the dim sea
+    And all dishevelled wandering stars.
     """
   end
 end
