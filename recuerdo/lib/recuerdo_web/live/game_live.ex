cebdfed128bd20cac72d3237b51c700be337a144
Bruce Tate <bruce@rapidred.com>
2020-06-25 11:18:43 -0400

    adding a template

--- recuerdo/lib/recuerdo_web/live/game_live.ex
+++ recuerdo/lib/recuerdo_web/live/game_live.ex
@@ -2,20 +2,9 @@ defmodule RecuerdoWeb.GameLive do
   use RecuerdoWeb, :live_view
   alias Recuerdo.Model
 
-  @impl true
   @spec mount(any, any, Phoenix.LiveView.Socket.t()) :: {:ok, any}
   def mount(_params, _session, socket) do
     {:ok, new_eraser(socket)}
-  end
-
-  def render(assigns) do
-    ~L"""
-    <h1>Recuerdo!</h1>
-    <p><%= @eraser.current_text %></p>
-    <button phx-click="erase">Erase!</button>
-    <button <%= disabled?(@eraser) %> phx-click="play_again">Play Again</button>
-    <pre><%= inspect @eraser %></pre>
-    """
   end
 
   def handle_event("erase", _, socket) do
