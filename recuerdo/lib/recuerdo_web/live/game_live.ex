007357fb25b3657660d6e29908fe3aff75c5d496
Adolfo Neto <adolfo.usp@gmail.com>
2020-06-25 11:53:26 -0300

    Play Again button added

--- recuerdo/lib/recuerdo_web/live/game_live.ex
+++ recuerdo/lib/recuerdo_web/live/game_live.ex
@@ -5,11 +5,7 @@ defmodule RecuerdoWeb.GameLive do
   @impl true
   @spec mount(any, any, Phoenix.LiveView.Socket.t()) :: {:ok, any}
   def mount(_params, _session, socket) do
-    text = Speech.random_passage()
-    steps = 5
-    eraser = Model.new(text, steps)
-    socket = assign(socket, eraser: eraser)
-    {:ok, socket}
+    {:ok, new_eraser(socket)}
   end
 
   def render(assigns) do
@@ -17,6 +13,7 @@ defmodule RecuerdoWeb.GameLive do
     <h1>Recuerdo!</h1>
     <p><%= @eraser.current_text %></p>
     <button phx-click="erase">Erase!</button>
+    <button <%= disabled?(@eraser) %> phx-click="play_again">Play Again</button>
     <pre><%= inspect @eraser %></pre>
     """
   end
@@ -25,11 +22,28 @@ defmodule RecuerdoWeb.GameLive do
     {:noreply, socket |> erase()}
   end
 
+  def handle_event("play_again", _, socket) do
+    {:noreply, new_eraser(socket)}
+  end
+
   defp erase(socket) do
     new_eraser =
       socket.assigns.eraser
       |> Model.erase()
 
     assign(socket, eraser: new_eraser)
+  end
+
+  def new_eraser(socket) do
+    text = Speech.random_passage()
+    steps = 5
+    eraser = Model.new(text, steps)
+    assign(socket, eraser: eraser)
+  end
+
+  def disabled?(eraser) do
+    unless Model.finished?(eraser) do
+      "disabled"
+    end
   end
 end
